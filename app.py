96eddd2557758df64900c86e7ee18ce4d5142b87
kaoru-fsp <131385016+kaoru-fsp@users.noreply.github.com>
2026-06-01 00:33:48 +0900

    fix: itemにdnumbを追加

--- app.py
+++ app.py
@@ -343,6 +343,7 @@ def select_three_points(base_date=None, base_latlng=None, radius=None, place_nam
                 'url': view_image_url(d.get('Published', ''), d.get('PicFileName', '')),
                 'base_name': base_name,
             'maplink': d.get('MapLink', ''),
+            'dnumb': str(d.get('dNumb', '')),
             }
             if keyword and not (keyword in d.get('Subject','') or keyword in d.get('Place','')):
                 continue
